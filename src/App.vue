80a3197400756a35eaf81b57d19a6834352dc0b1
Chaimaa Zegoumou <chaimaafacezeg@gmail.com>
2020-11-01 00:40:07 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Doggo In a Stroller" src="./assets/doggo.jpg">
+  <Countdown/>
+</template>
+
+<script>
+import Countdown from './components/Countdown.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Countdown
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img {
+  height: 200px;
+  width: 200px;
+}
+</style>
---
+++ src/components/Countdown.vue
@@ -0,0 +1,89 @@
+<template>
+  <h1>Hello friends (｡◕‿‿◕｡) </h1>
+  Enter number of hours for countdown : 
+  <input v-model="hours">
+  <br>
+  Enter number of minutes for countdown : 
+  <input v-model="mins">
+  <br>
+
+  Enter number of seconds for countdown : 
+  <input v-model="secs">
+  <br>
+  <br>
+
+  <button @click="launchCountdown">Launching countdown </button>
+  <div v-if="show">
+    {{countingHr}} : {{countingMin}} : {{countingSec}}
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'Countdown',
+  data(){
+    return {
+      hours : 0,
+      mins: 0,
+      secs :0,
+      countingMin: 0,
+      countingHr : 0,
+      countingSec : 0,
+      show : false
+    }
+  },
+  methods :{
+      async launchCountdown(){
+      this.show = true;
+      console.log(this.show);
+      this.countingMin =this.mins;
+      this.countingHr =this.hours;
+      this.countingSec =this.secs;
+      console.log("here")
+      const timer = setInterval(()=>{
+        if(this.countingSec > 0){
+          this.countingSec=this.countingSec-1;
+      }
+      else {
+        if(this.countingMin > 0){
+          this.countingMin=this.countingMin-1;
+          this.countingSec = 59;
+        }
+        else{
+          if(this.countingHr> 0){
+            this.countingHr= this.countingHr-1;
+            this.countingMin= 59;
+          }
+          else{
+            clearInterval(timer);
+          }
+        }
+      }
+        console.log(this.countingMin)
+        console.log(this.countingHr)
+        console.log(this.countingSec)
+      },1000);
+        console.log("done");
+    return this.show;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
